c3523fd5431c53ce24cef2699eccdaf57a9a17c4
Muhammad Sabilil Fajri <muhamadfajri943@gmail.com>
2026-03-11 17:25:50 +0700

    add: lastmod on pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,70 +7,58 @@ const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.arutalalab.com
 /** Revalidate sitemap every hour */
 export const revalidate = 3600
 
-// <!-- <?xml version="1.0" encoding="UTF-8"?>
-// <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-// <sitemap><loc>https://www.arutalalab.com/sitemap-0.xml</loc></sitemap>
-// </sitemapindex> -->
-
-// <?xml version="1.0" encoding="UTF-8"?>
-// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-// <url><loc>https://www.arutalalab.com/articles/blockchain-introduction</loc><lastmod>2026-03-10T04:33:06.449Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/articles</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/courses/back-end-golang-using-docker</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/courses/coding-express</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/courses</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/it-education</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/kontak</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/mitra</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/profile</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/resources</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// <url><loc>https://www.arutalalab.com/software-services</loc><lastmod>2026-03-10T04:33:06.451Z</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>
-// </urlset>
-
 const staticRoutes: MetadataRoute.Sitemap = [
     {
         url: SITE_URL,
+        lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 1,
     },
     {
         url: `${SITE_URL}/articles`,
+        lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.8,
     },
     {
         url: `${SITE_URL}/courses`,
+        lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.8,
     },
     {
         url: `${SITE_URL}/it-education`,
+        lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.7,
     },
     {
         url: `${SITE_URL}/resources`,
+        lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.7,
     },
     {
         url: `${SITE_URL}/software-services`,
+        lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.7,
     },
     {
         url: `${SITE_URL}/profile`,
+        lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.6,
     },
     {
         url: `${SITE_URL}/mitra`,
+        lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.6,
     },
     {
         url: `${SITE_URL}/kontak`,
+        lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 0.5,
     },
